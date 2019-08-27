2e8f5405474c015aaaeb160fa8ebfd097225a144
PerervaKostya <pererva.kostya@outlook.com>
2019-08-27 13:00:39 +0300

    popup return order - theicon

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
     $('.form-return-select').niceSelect();
 
+    //open popup return
+    $('[data-button="open-popup-return"]').click(function () {
+        openPopupReturn();
+    });
 
+    $('[data-submit="form-return"]').click(function (e) {
+        e.preventDefault();
+        openPopupSuccessReturn()
+    });
+
+    $('[data-close="form-return"]').click(function () {
+        closePopupReturn()
+    });
+
+    $('[data-close="return-success"]').click(function () {
+        closePopupSuccessReturn()
+    });
+
+    function closePopupReturn() {
+        $('[data-popup="return"]').fadeOut();
+    }
+
+    function openPopupReturn() {
+        $('[data-popup="return"]').fadeIn();
+    }
+
+    function openPopupSuccessReturn() {
+        $('[data-popup="return"]').fadeOut();
+        $('[data-popup="return-success"]').fadeIn();
+    }
+
+    function closePopupSuccessReturn() {
+        $('[data-popup="return-success"]').fadeOut();
+    }
 });
